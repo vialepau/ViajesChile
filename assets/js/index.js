731a4fa851d1e94f1393784b443256d0d4717f83
Paulina Viale <vialepau@gmail.com>
2021-07-01 20:45:56 -0400

    se cambia color de navbar, se soluciona error en cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,30 +11,35 @@
 //         );
 //     });
 // });
-/////////////////////////////////////////////////////
+
 // $(document).ready(function(){
-//     // Add smooth scrolling to all links
+   
 //     $("a").on('click', function(event) {
   
-//       // Make sure this.hash has a value before overriding default behavior
 //       if (this.hash !== "") {
-//         // Prevent default anchor click behavior
+        
 //         event.preventDefault();
   
-//         // Store hash
-//         var hash = this.hash;
-  
-//         // Using jQuery's animate() method to add smooth page scroll
-//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       
+//         var hash = this.hash;  
+    
 //         $('html, body').animate({
 //           scrollTop: $(hash).offset().top
 //         }, 800, function(){
   
-//           // Add hash (#) to URL when done scrolling (default click behavior)
 //           window.location.hash = hash;
 //         });
-//       } // End if
+//       } 
 //     });
 //   });
-////////////////////////////////////////////////////////
-// Select all links with hashes
+
+  $("a").on('click', function (event) {
+
+    event.preventDefault();
+    
+    var hash = this.hash;
+    
+    $('html').animate({
+    scrollTop: $(hash).offset().top - 80
+    }, 1000)
+    });
