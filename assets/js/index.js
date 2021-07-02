edaa6c012fcac027e260a09cf2cbde984f2ef82c
Paulina Viale <vialepau@gmail.com>
2021-07-01 21:39:19 -0400

    min fix en index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,4 @@
-// $(document).ready(function () {
-//     $("a").click(function (event) {
-//         event.preventDefault();
-
-//         var miHash = this.hash;
-//         $("html").animate(
-//             {
-//                 scrollTop: $(miHash).offset().top
-//             },
-//             800
-//         );
-//     });
-// });
-
-// $(document).ready(function(){
-   
-//     $("a").on('click', function(event) {
-  
-//       if (this.hash !== "") {
-        
-//         event.preventDefault();
-  
-       
-//         var hash = this.hash;  
-    
-//         $('html, body').animate({
-//           scrollTop: $(hash).offset().top
-//         }, 800, function(){
-  
-//           window.location.hash = hash;
-//         });
-//       } 
-//     });
-//   });
-
-  $("a").on('click', function (event) {
+ $("a").on('click', function (event) {
 
     event.preventDefault();
     
@@ -42,4 +7,5 @@
     $('html').animate({
     scrollTop: $(hash).offset().top - 80
     }, 1000)
+    
     });
